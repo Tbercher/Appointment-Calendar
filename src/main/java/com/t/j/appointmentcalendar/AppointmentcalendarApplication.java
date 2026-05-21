438645054c13526caa194a8c4793939466b0f96b
Trevor Bercher <trevorbercher@gmail.com>
2026-05-21 17:59:27 -0400

    Feat: Created foundation for springboot

---
+++ src/main/java/com/t/j/appointmentcalendar/AppointmentcalendarApplication.java
@@ -0,0 +1,13 @@
+package com.t.j.appointmentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentcalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentcalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t/j/appointmentcalendar/AppointmentcalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t.j.appointmentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentcalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
