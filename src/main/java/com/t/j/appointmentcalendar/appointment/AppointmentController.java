6078972f343279c9c73b75c3c5e2a59f979eb0cf
Trevor Bercher <trevorbercher@gmail.com>
2026-05-21 19:28:04 -0400

    Update AppointmentController.java

--- src/main/java/com/t/j/appointmentcalendar/appointment/AppointmentController.java
+++ src/main/java/com/t/j/appointmentcalendar/appointment/AppointmentController.java
@@ -2,7 +2,6 @@ package com.t.j.appointmentcalendar.appointment;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.ArrayList;
