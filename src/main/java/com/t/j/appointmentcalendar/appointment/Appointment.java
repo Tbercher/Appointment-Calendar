560b3bc469afad7c63dc246f26f6aabedabfee14
Trevor Bercher <trevorbercher@gmail.com>
2026-05-23 21:21:29 -0400

    Feat: changed up a few things on Appointment,,java

--- src/main/java/com/t/j/appointmentcalendar/appointment/Appointment.java
+++ src/main/java/com/t/j/appointmentcalendar/appointment/Appointment.java
@@ -1,22 +1,74 @@
 package com.t.j.appointmentcalendar.appointment;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.t.j.appointmentcalendar.user.UserAccount;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
-import java.time.LocalTime;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+import java.time.LocalDateTime;
 
+
+
+
+@Entity
 public class Appointment {
-    private Long id; //Unique user specific identifiers
-    private String title; // user appointment name/title example: how to make a grilled cheese lessons
-    private LocalTime startTime; // time will be in military time HH:MM
-    private LocalTime endTime; // the two times represent the range in which a user can post their "availability" range
-    private Boolean taken; // indicates if appointment is taken
+    @NotBlank
+    private String username;
+    @NotBlank
+    private LocalDateTime startTime; // beginning time for appointment time slot
+    @NotBlank
+    private LocalDateTime endTime; // end time for appointment time slot
+    @NotBlank
+    private String appointmentTitle; // title of appointment
+    @NotBlank
+    private String appointmentDescription; // description
 
+    // Constructor
+    public Appointment(UserAccount user, LocalDateTime startTime, LocalDateTime endTime, String appointmentTitle, String appointmentDescription){
+        username = user.getUsername();
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.appointmentTitle = appointmentTitle;
+        this.appointmentDescription = appointmentDescription;
+    }
+
+    // No args Constructor
+    public Appointment(){
+
+    }
+
+    public LocalDateTime getStartTime(){return startTime;}
+
+    public void setStartTime(LocalDateTime startTime){
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime(){
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime){
+        this.endTime = endTime;
+    }
+
+
+
+    public String getAppointmentTitle() {
+        return appointmentTitle;
+    }
+
+    public void setAppointmentTitle(String appointmentTitle) {
+        this.appointmentTitle = appointmentTitle;
+    }
+
+    public String getAppointmentDescription(){
+        return appointmentDescription;
+    }
+
+    public void setAppointmentDescription(String appointmentDescription){
+        this.appointmentDescription = appointmentDescription;
+    }
 }
 
 
--- src/main/java/com/t/j/appointmentcalendar/appointment/AppointmentController.java
+++ src/main/java/com/t/j/appointmentcalendar/appointment/AppointmentController.java
@@ -1,32 +1,16 @@
 package com.t.j.appointmentcalendar.appointment;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalTime;
-import java.util.List;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+
 
 @RestController
-@RequestMapping("/api/v1/appointments")
-
+@RequestMapping("/vi/api/appointment")
 public class AppointmentController {
 
-    // Temporary in memory appointment list
-    private List<Appointment> appointments = new ArrayList<>();
-    private Long nextId = 1L;
-
-    @GetMapping
-    public List<Appointment> getAllAppointments(){
-        return appointments;
-    }
-
-    @PostMapping
-    public Appointment createAppointment(@RequestBody Appointment appointment){
-        appointment.setId(nextId++);
-        appointment.setStartTime(LocalTime.now());
-        appointment.setEndTime(LocalTime.now().plusHours(2)); // adds two hours acts as a placeholder for an appointment time slot
-        appointments.add(appointment);
-
-        return appointment;
-    }
+    
 }
