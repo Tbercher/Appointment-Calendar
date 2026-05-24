56e430b3667b5682baa94634369392285c8ab517
Trevor Bercher <trevorbercher@gmail.com>
2026-05-23 21:47:37 -0400

    Started implementing Appointment controller and appointment services will finish later

--- src/main/java/com/t/j/appointmentcalendar/appointment/AppointmentController.java
+++ src/main/java/com/t/j/appointmentcalendar/appointment/AppointmentController.java
@@ -6,11 +6,25 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/vi/api/appointment")
 public class AppointmentController {
 
+    private final AppointmentServices appointmentServices;
 
+    public AppointmentController(AppointmentServices appointmentServices){
+        this.appointmentServices = appointmentServices;
+    }
+
+    // Gets all appointments
+    @GetMapping("/allAppointments")
+    public List<Appointment> getAppointments(){
+        return appointmentServices.getAllAppointments();
+    }
+
+    // Get to a specific appointment
+    @GetMapping("/")
 }
